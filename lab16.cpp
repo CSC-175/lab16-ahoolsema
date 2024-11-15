80f415cf04b3996f88f2e62b1aae0672b52b30a1
Austin Hoolsema <ahoolsem@umich.edu>
2024-11-14 23:50:08 -0500

    Attempt 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,22 @@
 // Code to implement the gcf function goes here
+double gcf(double a, double b) {
+    int bigger;
+    int smaller;
+    int remainder;
+    if (a > b) {
+        bigger = a;
+        smaller = b;
+    }
+    else {
+        smaller = a;
+        bigger = b;
+    }
+
+    remainder = bigger % smaller;
+    if(remainder == 0) {
+        return smaller;
+    }
+    else {
+        return gcf(smaller, remainder);
+    }
+}
